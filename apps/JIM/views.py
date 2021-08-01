a80875295ac35a938a2487b4dad8c915c68b412a
James Jung <therealjamesjung@gmail.com>
2021-08-01 17:21:30 +0900

    Division List API

--- apps/JIM/views.py
+++ apps/JIM/views.py
@@ -1,5 +1,6 @@
 from rest_framework.generics import ListCreateAPIView
 from django_filters.rest_framework import DjangoFilterBackend
+from django.utils.datastructures import MultiValueDictKeyError as MVDKError
 
 from .serializer import (ClientSerializer, UnitSerializer,
                          DivisionSerializer, DrawingSerializer)
@@ -26,3 +27,16 @@ class DrawingListCreateAPIView(ListCreateAPIView):
 class DivisionListCreateAPIView(ListCreateAPIView):
     serializer_class = DivisionSerializer
     queryset = Division.objects.all()
+    pagination_class = None
+
+    def get_queryset(self):
+
+        try:
+            query_param = self.request.query_params['client']
+            queryset = Division.objects.filter(
+                unit__drawing__client=query_param)
+        except MVDKError:
+            queryset = Division.objects.all()
+
+        return queryset.distinct().order_by(
+            'main_division', 'name', 'sub_division')
