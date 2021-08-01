55531ab183f1bacbbea0bd794036e823c9a9802d
James Jung <therealjamesjung@gmail.com>
2021-08-01 17:44:15 +0900

    Divison Create API

--- apps/JIM/views.py
+++ apps/JIM/views.py
@@ -1,6 +1,10 @@
 from rest_framework.generics import ListCreateAPIView
+from rest_framework.response import Response
+from rest_framework.status import (
+    HTTP_201_CREATED as _201,
+    HTTP_400_BAD_REQUEST as _400)
+
 from django_filters.rest_framework import DjangoFilterBackend
-from django.utils.datastructures import MultiValueDictKeyError as MVDKError
 
 from .serializer import (ClientSerializer, UnitSerializer,
                          DivisionSerializer, DrawingSerializer)
@@ -30,13 +34,29 @@ class DivisionListCreateAPIView(ListCreateAPIView):
     pagination_class = None
 
     def get_queryset(self):
-
-        try:
-            query_param = self.request.query_params['client']
+        query_param = self.request.query_params.get('client', None)
+        if query_param:
             queryset = Division.objects.filter(
                 unit__drawing__client=query_param)
-        except MVDKError:
+        else:
             queryset = Division.objects.all()
 
         return queryset.distinct().order_by(
             'main_division', 'name', 'sub_division')
+
+    def create(self, request, *args, **kwargs):
+
+        obj = Division.objects.filter(
+            name=request.data.get('name', None),
+            main_division=request.data.get('main_division', None),
+            sub_division=request.data.get('sub_division', None)
+        )
+        if obj:
+            request.data['message'] = 'This division already exists!'
+            return Response(request.data, status=_400)
+        else:
+            serializer = self.get_serializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            self.perform_create(serializer)
+            headers = self.get_success_headers(serializer.data)
+            return Response(serializer.data, status=_201, headers=headers)
