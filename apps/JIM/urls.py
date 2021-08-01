691e89bfb7d0d4f20cc5df0e0927a098ffc13c01
James Jung <therealjamesjung@gmail.com>
2021-08-01 12:39:52 +0900

    Add APIViews to url patterns

--- apps/JIM/urls.py
+++ apps/JIM/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from .views import ClientListCreateAPIView, UnitListCreateAPIView
+from .views import (ClientListCreateAPIView, UnitListCreateAPIView,
+                    DrawingListCreateAPIView, DivisionListCreateAPIView)
 
 
 urlpatterns = [
     path('client/', ClientListCreateAPIView.as_view()),
-    path('unit/', UnitListCreateAPIView.as_view())
+    path('unit/', UnitListCreateAPIView.as_view()),
+    path('drawing/', DrawingListCreateAPIView.as_view()),
+    path('division/', DivisionListCreateAPIView.as_view())
 ]
