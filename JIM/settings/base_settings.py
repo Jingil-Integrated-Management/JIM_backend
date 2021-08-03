bafc17050983aed32247b229550534779d939e5b
James Jung <therealjamesjung@gmail.com>
2021-08-04 00:23:27 +0900

    Enable CORS headers

--- JIM/settings/base_settings.py
+++ JIM/settings/base_settings.py
@@ -16,6 +16,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'corsheaders',
     'django_filters',
 
     'apps.JIM',
@@ -29,6 +30,7 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -37,6 +39,9 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'JIM.urls'
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
 
 TEMPLATES = [
     {
