235573e8822054769457787b48104b46de411c88
James Jung <therealjamesjung@gmail.com>
2021-08-05 14:27:26 +0900

    Set null on Client FK

--- apps/Drawing/models.py
+++ apps/Drawing/models.py
@@ -1,5 +1,4 @@
-import datetime
-
+from django.utils import timezone
 from django.db import models
 
 from apps.Client.models import Client
@@ -7,9 +6,9 @@ from apps.Client.models import Client
 
 class Drawing(models.Model):
     name = models.CharField(max_length=256, primary_key=True)
-    created_at = models.DateField(default=datetime.date.today())
+    created_at = models.DateField(default=timezone.now)
     client = models.ForeignKey(
-        Client, on_delete=models.CASCADE, related_name='drawing')
+        Client, on_delete=models.SET_NULL, related_name='drawing', null=True)
 
     def __str__(self):
         return self.name
