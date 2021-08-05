e05f916efed2911dc2245a008038d3e0539a35e3
James Jung <therealjamesjung@gmail.com>
2021-08-05 15:32:54 +0900

    Fix bugs on default for DateField

--- apps/Drawing/models.py
+++ apps/Drawing/models.py
@@ -1,4 +1,4 @@
-from django.utils import timezone
+import datetime
 from django.db import models
 
 from apps.Client.models import Client
@@ -6,7 +6,7 @@ from apps.Client.models import Client
 
 class Drawing(models.Model):
     name = models.CharField(max_length=256, primary_key=True)
-    created_at = models.DateField(default=timezone.now)
+    created_at = models.DateField(default=datetime.date.today)
     client = models.ForeignKey(
         Client, on_delete=models.SET_NULL, related_name='drawing', null=True)
 
