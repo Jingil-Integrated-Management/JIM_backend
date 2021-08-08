3fb45c450ef71c02babfe0cbb1d782355306d938
James Jung <therealjamesjung@gmail.com>
2021-08-08 19:25:27 +0900

    Serialize additional data to Part API

--- apps/Part/serializers.py
+++ apps/Part/serializers.py
@@ -1,12 +1,14 @@
 from rest_framework import serializers
+from rest_framework.fields import CharField
 from .models import Part
 
 
 class PartSerializer(serializers.ModelSerializer):
+    division_name = CharField(source='division.get_full_division')
+    drawing_name = CharField(source='drawing.name', required=False)
+    created_at = CharField(source='drawing.created_at')
 
     class Meta:
         model = Part
-        fields = '__all__'
-
-    def get_material(self, obj):
-        return self.get_material_display()
+        fields = ('id', 'x', 'y', 'z', 'price', 'material', 'comment',
+                  'material', 'division_name', 'drawing_name', 'created_at')
