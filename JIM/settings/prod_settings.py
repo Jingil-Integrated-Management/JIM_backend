4f379733bd31d04ececf6bcd333074140afd0f78
James Jung <therealjamesjung@gmail.com>
2021-08-22 02:33:13 +0900

    Add psql settings for production

--- JIM/settings/prod_settings.py
+++ JIM/settings/prod_settings.py
@@ -5,3 +5,14 @@ import os
 SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = True  # TODO False on actual production
 ALLOWED_HOSTS = ['34.64.103.154', 'jim-backend.duckdns.org']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'jim-backend',
+        'USER': 'postgres',
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': '34.64.207.136',
+        'PORT': '5432',
+    }
+}
