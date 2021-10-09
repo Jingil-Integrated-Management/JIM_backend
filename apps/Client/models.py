3db4bab394ef6badf7ad0ae558e3821f9a9cac6d
James Jung <therealjamesjung@gmail.com>
2021-10-09 17:16:56 +0900

    Add fields to Client

--- apps/Client/models.py
+++ apps/Client/models.py
@@ -2,7 +2,20 @@ from django.db import models
 
 
 class Client(models.Model):
+    business_id = models.CharField(max_length=256, unique=True)
     name = models.CharField(max_length=256)
+    representative = models.CharField(max_length=256, null=True, blank=True)
+    tel = models.CharField(max_length=128, null=True, blank=True)
+    fax = models.CharField(max_length=128, null=True, blank=True)
+    address = models.CharField(max_length=512, null=True, blank=True)
+    note = models.TextField()
+    manager = models.CharField(max_length=128, null=True, blank=True)
+    manager_tel = models.CharField(max_length=128, null=True, blank=True)
+    manager_phone = models.CharField(max_length=128, null=True, blank=True)
+    primary_bank = models.CharField(max_length=128, null=True, blank=True)
+    primary_bank_account = models.CharField(
+        max_length=256, null=True, blank=True)
+    bank_account_name = models.CharField(max_length=128, null=True, blank=True)
 
     def __str__(self):
         return self.name
