4ff11b76a4ec8d80cf5d1c8d42031d60fae2928f
James Jung <therealjamesjung@gmail.com>
2021-10-12 22:28:38 +0900

    Fix bugs on windows env

--- manage.py
+++ manage.py
@@ -6,9 +6,10 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    settings = 'JIM.settings.dev_settings' if sys.platform == 'darwin' else 'JIM.settings.prod_settings'
+    # settings = 'JIM.settings.dev_settings' if sys.platform == 'darwin' else 'JIM.settings.prod_settings'
 
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings)
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                          'JIM.settings.dev_settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
