99eecc1c0605b13ceeea6690c1d3974102293d9a
James Jung <therealjamesjung@gmail.com>
2021-10-30 13:15:27 +0900

    Remove unused fields

--- apps/Part/serializers.py
+++ apps/Part/serializers.py
@@ -13,9 +13,8 @@ class OutSourceSerializer(serializers.ModelSerializer):
 class PartSerializer(serializers.ModelSerializer):
     division_name = CharField(
         source='division.get_full_division', read_only=True)
-    drawing_file = CharField(source='drawing.get_file', read_only=True)
+    drawing_file = CharField(source='get_file', read_only=True)
     created_at = CharField(source='drawing.created_at', read_only=True)
-    type = CharField(source='get_type', read_only=True)
     outsource_info = OutSourceSerializer(source='outsource', read_only=True)
 
     class Meta:
