bf70c9413c7ab4ade919c38ed33d7c9f1cd842b5
James Jung <therealjamesjung@gmail.com>
2021-11-23 17:06:42 +0900

    Statistics API

--- apps/drawing/urls.py
+++ apps/drawing/urls.py
@@ -2,11 +2,14 @@ from django.urls import path
 
 from .views import (DrawingListCreateAPIView,
                     DrawingRetrieveUpdateDestroyAPIView,
-                    DashboardAPIView)
+                    DashboardAPIView,
+                    StatisticsAPIView,
+                    )
 
 
 urlpatterns = [
     path('drawing/', DrawingListCreateAPIView.as_view()),
     path('drawing/<drawing_pk>', DrawingRetrieveUpdateDestroyAPIView.as_view()),
     path('dashboard/', DashboardAPIView.as_view()),
+    path('stats/', StatisticsAPIView.as_view()),
 ]
--- apps/drawing/views.py
+++ apps/drawing/views.py
@@ -1,17 +1,26 @@
 import itertools
 import operator
+import calendar
 
+from django.db.models import Sum, F, IntegerField
+from django.db.models.functions import Cast
+
+from rest_framework.views import APIView
 from rest_framework.generics import (
     ListAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView)
 from rest_framework import filters
 from rest_framework.response import Response
 
+
 from django_filters.rest_framework import DjangoFilterBackend
+
+from apps.part.models import Part
 
 from .serializers import DrawingReadSerializer, DrawingWriteSerializer
 from .models import Drawing
 from .filters import DrawingFilter
 
+from utils.utils import to_int
 
 class DrawingListCreateAPIView(ListCreateAPIView):
     queryset = Drawing.objects.filter(
@@ -65,3 +74,39 @@ class DashboardAPIView(ListAPIView):
         serializer = self.get_serializer(queryset, many=True)
         self.group_by_client(serializer.data)
         return Response(self.group_by_client(serializer.data))
+
+
+class StatisticsAPIView(APIView):
+    pagination_class = None
+
+    def get(self, request):
+        year, month = request.query_params['date'].split('-')
+        queryset = Drawing.objects.filter(client=request.query_params['client']).filter(
+            created_at__gte='{}-{}-{}'.format(year, month, '01'),
+            created_at__lte='{}-{}-{}'.format(year, month, calendar.monthrange(int(year), int(month))[1])
+            ).prefetch_related('parts')
+
+        os_info = Part.objects.exclude(outsource=None).filter(drawing__in=queryset).aggregate(
+        os_revenue=Sum(Cast(F('price'), output_field=IntegerField()) * F('quantity')),
+        materials=Sum(Cast(F('outsource__material_price'), output_field=IntegerField()) * F('quantity')),
+        millings=Sum(Cast(F('outsource__milling_price'), output_field=IntegerField()) * F('quantity')),
+        wires=Sum(Cast(F('outsource__wire_price'), output_field=IntegerField()) * F('quantity')),
+        heat_treats=Sum(Cast(F('outsource__heat_treat_price'), output_field=IntegerField()) * F('quantity'))
+        )
+    
+        pol_info = Part.objects.filter(outsource=None).filter(drawing__in=queryset).aggregate(
+        pol_revenue=Sum(Cast(F('price'), output_field=IntegerField()) * F('quantity')),
+        )
+
+        result = {**os_info, **pol_info}
+        result['total_revenue'] = to_int(result['os_revenue']) + to_int(result['pol_revenue'])
+        result['os_profit'] = to_int(result['os_revenue']) \
+            - to_int(result['materials']) \
+            - to_int(result['millings']) \
+            - to_int(result['wires']) \
+            - to_int(result['heat_treats'])
+
+        result['client'] = queryset.first().client.name
+        result['date'] = '{}-{}'.format(year, month)
+
+        return Response(result)
