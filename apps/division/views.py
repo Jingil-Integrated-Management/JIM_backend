79f9a991d9f1ebdbdaff028539528eca0e60d6ef
James Jung <therealjamesjung@gmail.com>
2021-12-04 23:03:06 +0900

    fix bugs on duplicate check on division API

--- apps/division/views.py
+++ apps/division/views.py
@@ -1,7 +1,9 @@
 from django.db.models import query
-from rest_framework.generics import ListAPIView, ListCreateAPIView, UpdateAPIView
+from django.db.models.query_utils import Q
+from rest_framework.generics import ListAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.exceptions import ValidationError
 
 from django_filters.rest_framework import DjangoFilterBackend
 
@@ -28,9 +30,14 @@ class DivisionListCreateAPIView(ListCreateAPIView):
     def create(self, request, *args, **kwargs):
 
         obj = Division.objects.filter(
-            main_division=request.data.get('main_division', None),
-            sub_division=request.data.get('sub_division', None),
-        )
+            main_division=request.data.get('main_division', None))
+        sub_division = request.data.get('sub_division', None)
+
+        if not sub_division:
+            obj = obj.filter(sub_division__isnull=True)
+        else:
+            obj = obj.filter(sub_division=sub_division)
+
         if obj:
             request.data['message'] = 'This division already exists!'
             return Response(request.data, status=status.HTTP_400_BAD_REQUEST)
@@ -45,24 +52,38 @@ class DivisionListCreateAPIView(ListCreateAPIView):
                 headers=headers)
 
 
-class DivisionUpdateAPIView(UpdateAPIView):
+class DivisionRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
     queryset = Division.objects.all()
     serializer_class = DivisionSerializer
     lookup_url_kwarg = 'division_pk'
 
     def update(self, request, *args, **kwargs):
 
-        obj = Division.objects.filter(
-            main_division=request.data.get('main_division', None),
-            sub_division=request.data.get('sub_division', None),
-        )
+        main_division = self.request.data.get('main_division', None)
+        sub_division = self.request.data.get('sub_division', None)
+        client = self.request.data.get('client', None)
 
-        if obj:
+        instance = self.get_object()
+        queryset = Division.objects.all()
+
+        if main_division:
+            queryset = queryset.filter(main_division=main_division)
+        else:
+            queryset = queryset.filter(main_division=instance.main_division)
+        if sub_division:
+            queryset = queryset.filter(sub_division=sub_division)
+        else:
+            queryset = queryset.filter(sub_division=instance.sub_division)
+        if client:
+            queryset = queryset.filter(client=client)
+        else:
+            queryset = queryset.filter(client=instance.client)
+
+        if queryset:
             request.data['message'] = 'This division already exists!'
             return Response(request.data, status=status.HTTP_400_BAD_REQUEST)
         else:
             partial = kwargs.pop('partial', False)
-            instance = self.get_object()
             serializer = self.get_serializer(
                 instance, data=request.data, partial=partial)
             serializer.is_valid(raise_exception=True)
@@ -76,4 +97,3 @@ class MainDivisionListAPIView(ListAPIView):
     serializer_class = MainDivisionSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['client']
-
