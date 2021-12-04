9045a4a673af102a5e444601b449fc0aef1dc159
James Jung <therealjamesjung@gmail.com>
2021-12-05 02:20:02 +0900

    remove fields on client

--- apps/client/models.py
+++ apps/client/models.py
@@ -2,20 +2,7 @@ from django.db import models
 
 
 class Client(models.Model):
-    business_id = models.CharField(max_length=256, unique=True)
     name = models.CharField(max_length=256)
-    representative = models.CharField(max_length=256, null=True, blank=True)
-    tel = models.CharField(max_length=128, null=True, blank=True)
-    fax = models.CharField(max_length=128, null=True, blank=True)
-    address = models.CharField(max_length=512, null=True, blank=True)
-    note = models.TextField(null=True, blank=True)
-    manager = models.CharField(max_length=128, null=True, blank=True)
-    manager_tel = models.CharField(max_length=128, null=True, blank=True)
-    manager_phone = models.CharField(max_length=128, null=True, blank=True)
-    primary_bank = models.CharField(max_length=128, null=True, blank=True)
-    primary_bank_account = models.CharField(
-        max_length=256, null=True, blank=True)
-    bank_account_name = models.CharField(max_length=128, null=True, blank=True)
     is_pinned = models.IntegerField(default=0)
 
     def __str__(self):
